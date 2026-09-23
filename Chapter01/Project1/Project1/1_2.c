ee35febccdb977d718f2b8449a4fe09af78d76d7
ChoIkJun <89628722+ikkjun@users.noreply.github.com>
2026-09-23 12:30:21 +0900

    study C

---
+++ Chapter01/Project1/Project1/1_2.c
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+int main() {
+	printf("안녕하세요.");
+	printf("조익준이다.");
+
+	printf("안녕하세요.\n");
+	printf("조익준이다.");
+	return 0;
+}
---
+++ Chapter01/Project1/Project1/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+	printf("내 이름은 조익준이다.");
+	return 0;
+}
